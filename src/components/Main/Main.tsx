be332492e73cfd864d2ffbc5116fb7db874bf5bc
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2025-04-10 03:27:44 -0400

    update

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,5 @@
 import css from "./Main.module.css";
+import pic2 from "../../public/pic2.jpeg";
 
 
 export default function Main() {
@@ -10,7 +11,8 @@ return(
     <h1 className={css.h1}> Full Stack Developer</h1>
 <div className={css.content}>
 
-    <div className={css.pictureLeft}>КАРТИНКА</div>
+    
+    <img className={css.pic2} src={pic2} alt="code" />
     <div className={css.mainText}>Passionate and adaptable Front-end developer with a background in the arts. Long time use to live in Middle East, I can see different vision for developing and customer service. Dedicated to leveraging technology to create meaningful solutions. I thrive in dynamic environments and have a proven track record of successfully navigating and contributing to transformative periods. </div>
 </div>    
     </>
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,11 +1,37 @@
 import css from "./Skills.module.css";
+import html from "../../public/html.png";
+import csss from "../../public/css.png";
+import javascript from "../../public/javascript.png";
+import typescript from "../../public/typescript.png";
+import bootstrap from "../../public/bootstrap.png";
+import react from "../../public/react.png";
+import nodejs from "../../public/nodejs.png";
+import github from "../../public/github.png";
+import figma from "../../public/figma.png";
+
+
 
 
 
 export default function Skills(){
 
     return(
+<>
+        <h1 className={css.skills}>Skills</h1>
 
-        <div className={css.skills}> ICONS </div>
+        <div className={css.skillsIcons}>
+
+        <img src={html} alt="html" />
+        <img src={csss} alt="css" />
+        <img src={bootstrap} alt="bootstrap" />
+        <img src={javascript} alt="javascript" />
+        <img src={typescript} alt="typescript" />
+        <img src={react} alt="react" />
+        <img src={nodejs} alt="nodejs" />
+        <img src={github} alt="github" />
+        <img src={figma} alt="figma" />
+        </div>
+
+</>
     )
 }
