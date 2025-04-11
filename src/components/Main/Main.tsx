76090788f0e6bbc4bb49e1b28bd7123b6bec1e7e
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2025-04-11 03:19:49 -0400

    +

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,11 @@
 import css from "./Main.module.css";
-import pic2 from "../../public/pic2.jpeg";
+// import pic2 from "../../public/pic2.jpeg";
+
 
 
 export default function Main() {
 
+   
 
 
 return(
@@ -11,8 +13,8 @@ return(
     <h1 className={css.h1}> Full Stack Developer</h1>
 <div className={css.content}>
 
-    
-    <img className={css.pic2} src={pic2} alt="code" />
+    <div className={css.pic2}></div>
+   
     <div className={css.mainText}>Passionate and adaptable Front-end developer with a background in the arts. Long time use to live in Middle East, I can see different vision for developing and customer service. Dedicated to leveraging technology to create meaningful solutions. I thrive in dynamic environments and have a proven track record of successfully navigating and contributing to transformative periods. </div>
 </div>    
     </>
