fd4f4cd1260e1d3d8c74d1f7b283c44a08f976e0
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2025-04-19 02:14:54 -0400

    +

--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react";
 import css from "./Navigator.module.css";
+import AOS from "aos";
 
 export default function Navigator(){
 
+    useEffect(()=>{
+AOS.init({ duration: 2000, once: true });
+    },[]);
+
 
     return(
-        <nav className={css.nav}>
+        <nav className={css.nav} data-aos="fade-down">
          
             <a href="#main">About Me</a>
             <a href="#skills">Skills</a>
