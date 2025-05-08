29462764d659d00c232d1e2e2a1dff82d4816b1f
Ihor Cherviakov <118768294+cherviakow@users.noreply.github.com>
2025-05-08 01:58:53 -0400

    +

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -5,6 +5,13 @@ export default function Projects() {
     <>
       <div className={css.projDiv}>
         <h1 className={css.projects}>Projects</h1>
+        <p className={css.pp}>barber-lime.vercel.app</p>
+        <a className={css.pp} href="barber-lime.vercel.app"></a>
+        <a className={css.pp}
+              href="https://barber-lime.vercel.ap"
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
       </div>
     </>
   );
